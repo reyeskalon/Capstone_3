4e28f3c57935c6bf9521b3c246b3a1aadac7dab2
Kalon Reyes <reyeskalon@gmail.com>
2022-04-14 19:36:08 -0400

    Got admin page and brewery component communicating and passing brewery objects

--- vue/src/components/Brewery.vue
+++ vue/src/components/Brewery.vue
@@ -1,11 +1,27 @@
 <template>
-    <p>This is a brewery</p>
+    <div id="brewery-card">
+        <p>{{b.name}}</p>
+        <img :src="b.imgURL" alt="" id="brewery-img"/>
+        <p></p>
+    </div>
 </template>
+
 <script>
 export default {
-    
+    props: ['b']
 }
 </script>
 <style scoped>
+#brewery-card {
+    display: grid;
+    height: 400px;
+    padding: 50px;
+    margin: 10px;
+    background: lightgray;
+    border: red 1px solid;
 
+}
+#brewery-img {
+    height: 400px;
+}
 </style>
