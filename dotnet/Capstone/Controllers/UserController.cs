4395cf2939f74377d8b23ce883528c44ca17749a
David McKenna <dave43dm@gmail.com>
2022-04-15 10:45:00 -0400

    Made changes to the dotNet soultiuon to add /users end point.  Updated Database for the request_brewer property

---
+++ dotnet/Capstone/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Capstone.DAO;
+using Capstone.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Controllers
+{
+    [Route("[user]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserDao UserDao;
+        public UserController(IUserDao userDao)
+        {
+            UserDao = userDao;
+        }
+        [HttpGet("/users")]
+        public List<ReturnUser> GetAllUsers()
+        {
+            return UserDao.GetUsers();
+        }
+    }
+
+}
