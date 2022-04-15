3336e6da976160dcf8d4ecf962947580870fb358
Joe <joepbarry@gmail.com>
2022-04-15 11:00:58 -0400

    map updates

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,6 +4,56 @@
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
+
+    <!-- where my code begins  -->
+
+    <div>
+      <h1>Drop-A-Pint</h1>
+
+      <div id="googleMap"></div>
+    </div>
+
+    <!-- where my code ends -->
     <router-view />
   </div>
 </template>
+
+<!-- googlemap api -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAFvxjkFmpMry87XMtoIS54kWPHqkrAyxg&callback=myMap"></script>
+
+<script>
+export default {
+  // my code starts
+  function: {
+
+        myMap() {
+                var mapProp = {
+                    center: new google.maps.LatLng(40.440624, -79.995888),
+                    zoom:5,
+                };
+
+                var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+            }
+        }
+  // my code ends
+};
+</script>
+
+
+<style>
+/* Place holder for app view */
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+
+#map {
+  height: 90%;
+  width: 400px;
+}
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,75 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
-  </div>
+    <main>
+        <h1>Drop-A-Pint</h1>
+
+        <div id="googleMap"></div>
+  </main>
 </template>
 
-<script>
-export default {
-  name: "home"
-};
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAFvxjkFmpMry87XMtoIS54kWPHqkrAyxg&callback=myMap"></script>
+
+<script> 
+    export default {
+        name: "home",
+    
+    
+    function: {
+
+        myMap() {
+                var mapProp = {
+                    center: new google.maps.LatLng(40.440624, -79.995888),
+                    zoom:5,
+                };
+
+                var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+            }
+        }
+    }
 </script>
+
+<style>
+#googleMap{
+    height: 90%;
+    width: 400px;
+}
+/* .dropbtn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.drop-options {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.drop-options a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.drop-options a:hover {background-color: #ddd;}
+
+.dropdown:hover .drop-options {display: block;}
+
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
+
+
+#map {
+    height: 400px;
+    width: 100%;
+} */
+</style>
