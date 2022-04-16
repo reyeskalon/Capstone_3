d35686c83a61b9bc96dc184a79909dca33a2c916
Kalon Reyes <reyeskalon@gmail.com>
2022-04-16 15:27:41 -0400

    styled login page and navbar

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <features />
+    <featured-in />
   </div>
 </template>
 
 <script>
+import FeaturedIn from '../components/FeaturedIn.vue';
+import Features from '../components/Features.vue';
 
 export default {
+  components: { Features, FeaturedIn },
   name: "home",
 };
 </script>
